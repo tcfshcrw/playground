2f0b36da2ed9bf61fdb66c1ea44b7cdb8a470d72
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2024-08-15 08:21:56 +0800

    update

---
+++ ESP32/include/ABSOscillation.h
@@ -0,0 +1,532 @@
+#pragma once
+
+#include "DiyActivePedal_types.h"
+
+
+static const long ABS_ACTIVE_TIME_PER_TRIGGER_MILLIS = 100;
+static const long RPM_ACTIVE_TIME_PER_TRIGGER_MILLIS = 100;
+static const long BP_ACTIVE_TIME_PER_TRIGGER_MILLIS = 100;
+static const long WS_ACTIVE_TIME_PER_TRIGGER_MILLIS = 100;
+static const long CV_ACTIVE_TIME_PER_TRIGGER_MILLIS = 100;
+static int RPM_VALUE_LAST = 0;
+
+class ABSOscillation {
+private:
+  long _timeLastTriggerMillis;
+  long _absTimeMillis;
+  long _lastCallTimeMillis = 0;
+
+public:
+  ABSOscillation()
+    : _timeLastTriggerMillis(0)
+  {}
+
+public:
+  void trigger() {
+    _timeLastTriggerMillis = millis();
+  }
+  
+  void forceOffset(DAP_calculationVariables_st* calcVars_st, uint8_t absPattern, uint8_t absForceOrTarvelBit, float * absForceOffset, float * absPosOffset) {
+
+
+    long timeNowMillis = millis();
+    float timeSinceTrigger = (timeNowMillis - _timeLastTriggerMillis);
+    float absForceOffset_local = 0;
+
+    if (timeSinceTrigger > ABS_ACTIVE_TIME_PER_TRIGGER_MILLIS)
+    {
+      _absTimeMillis = 0;
+      absForceOffset = 0;
+    }
+    else
+    {
+      _absTimeMillis += timeNowMillis - _lastCallTimeMillis;
+      float absTimeSeconds = _absTimeMillis / 1000.0f;
+
+      // abs amplitude
+      float absAmp_fl32 = 0;
+      switch (absForceOrTarvelBit) {
+        case 0:
+          absAmp_fl32 = calcVars_st->absAmplitude;
+          break;
+        case 1:
+          absAmp_fl32 = calcVars_st->stepperPosRange * calcVars_st->absAmplitude / 100.;
+          break;
+        default:
+          break;
+      }
+
+
+      
+      switch (absPattern) {
+        case 0:
+          // sine wave pattern
+          absForceOffset_local =  absAmp_fl32 * sin(2 * PI * calcVars_st->absFrequency * absTimeSeconds);
+          break;
+        case 1:
+          // sawtooth pattern
+          if (calcVars_st->absFrequency > 0)
+          {
+            absForceOffset_local = absAmp_fl32 * fmod(absTimeSeconds, 1.0 / (float)calcVars_st->absFrequency) * (float)calcVars_st->absFrequency;
+          }
+          break;
+        default:
+          break;
+      }
+      
+      switch (absForceOrTarvelBit) {
+        case 0:
+          *absForceOffset = absForceOffset_local;
+          *absPosOffset = 0;
+          break;
+        case 1:
+          *absForceOffset = 0;
+          *absPosOffset = absForceOffset_local;
+          break;
+        default:
+          break;
+      }
+      
+    }
+
+    _lastCallTimeMillis = timeNowMillis;
+
+    return;
+    
+  }
+};
+
+class RPMOscillation {
+private:
+  long _timeLastTriggerMillis;
+  long _RPMTimeMillis;
+  long _lastCallTimeMillis = 0;
+  
+
+public:
+  RPMOscillation()
+    : _timeLastTriggerMillis(0)
+  {}
+  float RPM_value =0;
+  int32_t RPM_position_offset = 0;
+public:
+  void trigger() {
+    _timeLastTriggerMillis = millis();
+  }
+  
+  void forceOffset(DAP_calculationVariables_st* calcVars_st) {
+
+
+    long timeNowMillis = millis();
+    float timeSinceTrigger = (timeNowMillis - _timeLastTriggerMillis);
+    float RPMForceOffset = 0;
+    float RPM_max_freq = calcVars_st->RPM_max_freq;
+    float RPM_min_freq = calcVars_st->RPM_min_freq;
+    //float RPM_max =10;
+    float RPM_amp_base = calcVars_st->RPM_AMP;
+    float RPM_amp=0;
+    if(RPM_value==0)
+    {
+      RPM_min_freq=0;
+    }
+    RPM_amp=RPM_amp_base*(1+0.3*RPM_value/100);
+
+
+    float RPM_freq=constrain(RPM_value*(RPM_max_freq-RPM_min_freq)/100, RPM_min_freq, RPM_max_freq);
+    
+
+
+    if (timeSinceTrigger > RPM_ACTIVE_TIME_PER_TRIGGER_MILLIS)
+    {
+      _RPMTimeMillis = 0;
+      RPMForceOffset = RPM_VALUE_LAST;
+    }
+    else
+    {
+      _RPMTimeMillis += timeNowMillis - _lastCallTimeMillis;
+      float RPMTimeSeconds = _RPMTimeMillis / 1000.0f;
+
+      //RPMForceOffset = calcVars_st->absAmplitude * sin(calcVars_st->absFrequency * RPMTimeSeconds);
+      RPMForceOffset = RPM_amp * sin( 2*PI* RPM_freq* RPMTimeSeconds);
+    }
+
+    _lastCallTimeMillis = timeNowMillis;
+    RPM_VALUE_LAST=RPMForceOffset;
+    if (calcVars_st->Force_Range > 0)
+    {
+        RPM_position_offset = calcVars_st->stepperPosRange*(RPMForceOffset/calcVars_st->Force_Range);
+    }
+      //return RPMForceOffset;
+    
+
+  }
+};
+
+class BitePointOscillation {
+private:
+  long _timeLastTriggerMillis;
+  long _BiteTimeMillis;
+  long _lastCallTimeMillis = 0;
+  
+
+public:
+  BitePointOscillation()
+    : _timeLastTriggerMillis(0)
+  {}
+  //float RPM_value =0;
+  float BitePoint_Force_offset = 0;
+public:
+  void trigger() {
+    _timeLastTriggerMillis = millis();
+  }
+  
+  void forceOffset(DAP_calculationVariables_st* calcVars_st) {
+
+
+    long timeNowMillis = millis();
+    float timeSinceTrigger = (timeNowMillis - _timeLastTriggerMillis);
+    float BitePointForceOffset = 0;
+    float BP_freq = calcVars_st->BP_freq;
+    //float BP_freq = 15;
+    float BP_amp = calcVars_st->BP_amp;
+    //float BP_amp = 2;
+
+    if (timeSinceTrigger > BP_ACTIVE_TIME_PER_TRIGGER_MILLIS)
+    {
+      _BiteTimeMillis = 0;
+      BitePointForceOffset = 0;
+    }
+    else
+    {
+      _BiteTimeMillis += timeNowMillis - _lastCallTimeMillis;
+      float BPTimeSeconds = _BiteTimeMillis / 1000.0f;
+
+      //RPMForceOffset = calcVars_st->absAmplitude * sin(calcVars_st->absFrequency * RPMTimeSeconds);
+      BitePointForceOffset = BP_amp * sin( 2*PI* BP_freq* BPTimeSeconds);
+    }
+    BitePoint_Force_offset=BitePointForceOffset;
+    _lastCallTimeMillis = timeNowMillis;
+    //RPM_VALUE_LAST=RPMForceOffset;
+    
+    //return RPMForceOffset;
+    
+
+  }
+};
+
+// moving average filter:https://github.com/sebnil/Moving-Avarage-Filter--Arduino-Library-/tree/master
+
+#define MAX_DATA_POINTS 100
+class MovingAverageFilter
+{
+public:
+  //construct without coefs
+  MovingAverageFilter(unsigned int newDataPointsCount);
+  int dataPointsCount;
+  float process(float in);
+  
+
+private:
+  float values[MAX_DATA_POINTS];
+  int k; // k stores the index of the current array read to create a circular memory through the array
+  
+  float out;
+  int i; // just a loop counter
+};
+
+MovingAverageFilter::MovingAverageFilter(unsigned int newDataPointsCount)
+{
+  k = 0; //initialize so that we start to write at index 0
+  if (newDataPointsCount < MAX_DATA_POINTS)
+    dataPointsCount = newDataPointsCount;
+  else
+    dataPointsCount = MAX_DATA_POINTS;
+
+  for (i = 0; i < dataPointsCount; i++)
+  {
+    values[i] = 0; // fill the array with 0's
+  }
+}
+
+float MovingAverageFilter::process(float in)
+{
+  out = 0;
+
+  values[k] = in;
+  k = (k + 1) % dataPointsCount;
+
+  for (i = 0; i < dataPointsCount; i++)
+  {
+    out += values[i];
+  }
+
+  float retValue= 0;
+  if (dataPointsCount> 0)
+  {
+    retValue= out / dataPointsCount;
+  }
+  
+  return retValue;
+}
+
+MovingAverageFilter movingAverageFilter(100);
+// G force effect
+class G_force_effect
+{
+  public:
+  float G_value=0;
+  float G_force_raw=0;
+  float G_force=0;
+  
+
+  void forceOffset(DAP_calculationVariables_st* calcVars_st, uint8_t G_multi)
+  {
+    uint32_t Force_Range;
+    float G_multiplier=((float)G_multi)/100;
+    Force_Range=calcVars_st->Force_Range;
+    if(G_value==-128)
+    {
+      G_force_raw=0;
+
+    }
+    else
+    {
+      G_force_raw=10*(G_value)*G_multiplier/9.8;
+      //G_force_raw=constrain(G_force_raw,-1*Force_Range*0.25,Force_Range*0.25);
+    }
+
+    //apply filter
+    G_force=movingAverageFilter.process(G_force_raw);
+    //G_force=G_force_raw;
+    
+  }
+};
+//Wheel slip
+class WSOscillation {
+private:
+  long _timeLastTriggerMillis;
+  long _WSTimeMillis;
+  long _lastCallTimeMillis = 0;
+  
+
+public:
+  WSOscillation()
+    : _timeLastTriggerMillis(0)
+  {}
+  //float RPM_value =0;
+  float WS_Force_offset = 0;
+public:
+  void trigger() {
+    _timeLastTriggerMillis = millis();
+  }
+  
+  void forceOffset(DAP_calculationVariables_st* calcVars_st) {
+
+
+    long timeNowMillis = millis();
+    float timeSinceTrigger = (timeNowMillis - _timeLastTriggerMillis);
+    float WSForceOffset = 0;
+    float WS_freq = calcVars_st->WS_freq;
+    //float BP_freq = 15;
+    float WS_amp = calcVars_st->WS_amp;
+    //float BP_amp = 2;
+
+    if (timeSinceTrigger > WS_ACTIVE_TIME_PER_TRIGGER_MILLIS)
+    {
+      _WSTimeMillis = 0;
+      WSForceOffset = 0;
+    }
+    else
+    {
+      _WSTimeMillis += timeNowMillis - _lastCallTimeMillis;
+      float WSTimeSeconds = _WSTimeMillis / 1000.0f;
+
+      //RPMForceOffset = calcVars_st->absAmplitude * sin(calcVars_st->absFrequency * RPMTimeSeconds);
+      WSForceOffset = WS_amp * sin( 2*PI* WS_freq* WSTimeSeconds);
+      /*if (WS_freq > 0)
+      {
+        //WSForceOffset = WS_amp * fmod(WSTimeSeconds, 1.0 / (float)WS_freq) * WS_freq;
+        //WSForceOffset = WS_amp * (2*fmod(WSTimeSeconds, 1.0 / (float)WS_freq) * WS_freq-1);
+      }
+      */
+            
+          
+    }
+    WS_Force_offset=WSForceOffset;
+    _lastCallTimeMillis = timeNowMillis;
+    //RPM_VALUE_LAST=RPMForceOffset;
+    
+    //return RPMForceOffset;
+    
+
+  }
+};
+//Road impact
+MovingAverageFilter movingAverageFilter_roadimpact(100);
+class Road_impact_effect
+{
+  public:
+  float Road_Impact_force=0;
+  float Road_Impact_force_raw=0;
+  uint8_t Road_Impact_value=0;
+
+  void forceOffset(DAP_calculationVariables_st* calcVars_st, uint8_t Road_impact_multi)
+  {
+    uint32_t Force_Range;
+    float Road_multiplier=((float)Road_impact_multi)/100;
+    Force_Range=calcVars_st->Force_Range;
+    //Road_multiplier=0.1;
+    Road_Impact_force_raw=0.3*Road_multiplier*((float)Force_Range)*((float)Road_Impact_value)/100;
+
+    //apply filter
+    Road_Impact_force=movingAverageFilter_roadimpact.process(Road_Impact_force_raw);
+    
+    
+  }
+};
+//Wheel slip
+class Custom_vibration {
+private:
+  long _timeLastTriggerMillis;
+  long _CVTimeMillis;
+  long _lastCallTimeMillis = 0;
+  
+
+public:
+  Custom_vibration()
+    : _timeLastTriggerMillis(0)
+  {}
+  //float RPM_value =0;
+  float CV_Force_offset = 0;
+public:
+  void trigger() {
+    _timeLastTriggerMillis = millis();
+  }
+  
+  void forceOffset(float CV_freq, float CV_amp) {
+
+
+    long timeNowMillis = millis();
+    float timeSinceTrigger = (timeNowMillis - _timeLastTriggerMillis);
+    float CVForceOffset = 0;
+
+
+    if (timeSinceTrigger > CV_ACTIVE_TIME_PER_TRIGGER_MILLIS)
+    {
+      _CVTimeMillis = 0;
+      CVForceOffset = 0;
+    }
+    else
+    {
+      _CVTimeMillis += timeNowMillis - _lastCallTimeMillis;
+      float CVTimeSeconds = _CVTimeMillis / 1000.0f;
+
+      CVForceOffset = CV_amp/20.0f * sin( 2*PI* CV_freq* CVTimeSeconds);
+
+            
+          
+    }
+    CV_Force_offset=CVForceOffset;
+    _lastCallTimeMillis = timeNowMillis;
+    
+
+  }
+};
+MovingAverageFilter averagefilter_rudder(50);
+MovingAverageFilter averagefilter_rudder_force(50);
+class Rudder{
+  public:
+  int32_t Center_offset;
+  int32_t offset_raw;
+  int32_t offset_filter;
+  int32_t stepper_range;
+  int32_t dead_zone_upper;
+  int32_t dead_zone_lower;
+  int32_t dead_zone;
+  int16_t sync_pedal_position;
+  int16_t current_pedal_position;
+  float endpos_travel;
+  float force_range;  
+  float force_offset_raw;
+  float force_offset_filter;
+  float force_center_offset;
+  float position_ratio_sync;
+  float position_ratio_current;
+  int debug_count=0;
+
+  void offset_calculate(DAP_calculationVariables_st* calcVars_st)
+  {
+    current_pedal_position=calcVars_st->current_pedal_position;
+    position_ratio_sync=calcVars_st->Sync_pedal_position_ratio;
+    endpos_travel=(float)calcVars_st->stepperPosRange;
+    position_ratio_current=((float)(current_pedal_position-calcVars_st->stepperPosMin))/endpos_travel;    
+    dead_zone=20;
+    Center_offset=calcVars_st->stepperPosMin+ calcVars_st->stepperPosRange/2;
+    float center_deadzone = 0.51;
+    if(calcVars_st->Rudder_status)
+    {
+      if(position_ratio_sync>center_deadzone)
+      {
+        offset_raw=(int32_t)(-1*(position_ratio_sync-0.50)*endpos_travel);
+          
+      }
+      else
+      {
+        offset_raw=0;
+      }
+      if(calcVars_st->rudder_brake_status)
+      {
+        offset_raw=0;
+      }
+      offset_filter=averagefilter_rudder.process(offset_raw+Center_offset);
+    }
+    else
+    {
+      offset_filter=calcVars_st->stepperPosMin;
+    }
+
+  }
+  void force_offset_calculate(DAP_calculationVariables_st* calcVars_st)
+  {
+    dead_zone=20;
+    Center_offset=calcVars_st->stepperPosRange/2;
+    dead_zone_upper=Center_offset+dead_zone/2;
+    dead_zone_lower=Center_offset-dead_zone/2;
+    sync_pedal_position=calcVars_st->sync_pedal_position;
+    current_pedal_position=calcVars_st->current_pedal_position;
+    stepper_range=calcVars_st->stepperPosRange;
+    force_range=calcVars_st->Force_Range;
+    force_center_offset=force_range/2+calcVars_st->Force_Min;
+    endpos_travel=(float)calcVars_st->stepperPosRange;
+    //endpos_travel=((float)(calcVars_st->current_pedal_position-calcVars_st->stepperPosMin))/((float)calcVars_st->stepperPosRange);
+    position_ratio_sync=calcVars_st->Sync_pedal_position_ratio;
+    position_ratio_current=((float)(current_pedal_position-calcVars_st->stepperPosMin))/endpos_travel;
+    
+
+    float center_deadzone = 0.51;
+    if(calcVars_st->Rudder_status)
+    {
+      
+        
+        if(position_ratio_sync>center_deadzone)
+        {
+          force_offset_raw=(float)(-1*(position_ratio_sync-0.50)*force_range);
+          
+        }
+        else
+        {
+          force_offset_raw=0;
+        }
+        if(calcVars_st->rudder_brake_status)
+        {
+          force_offset_raw=0;
+        }
+     
+      force_offset_filter=averagefilter_rudder_force.process(force_offset_raw+force_center_offset);
+    }
+    else
+    {
+      force_offset_filter=0;
+    }
+  }
+};
---
+++ ESP32/include/ESPNOW_lib.h
@@ -0,0 +1,362 @@
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <Arduino.h>
+#include "ESPNowW.h"
+//#define ESPNow_debug
+uint8_t esp_master[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x31};
+//uint8_t esp_master[] = {0xdc, 0xda, 0x0c, 0x22, 0x8f, 0xd8}; // S3
+//uint8_t esp_master[] = {0x48, 0x27, 0xe2, 0x59, 0x48, 0xc0}; // S2 mini
+uint8_t Clu_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x32};
+uint8_t Gas_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+uint8_t Brk_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x34};
+uint8_t broadcast_mac[]={0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t esp_Host[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x35};
+uint8_t* Recv_mac;
+uint16_t ESPNow_send=0;
+uint16_t ESPNow_recieve=0;
+//bool MAC_get=false;
+bool ESPNOW_status =false;
+bool ESPNow_initial_status=false;
+bool ESPNow_update= false;
+bool ESPNow_no_device=false;
+bool ESPNow_config_request=false;
+bool ESPNow_restart=false;
+
+
+
+struct ESPNow_Send_Struct
+{ 
+  uint16_t pedal_position;
+  float pedal_position_ratio;
+};
+
+typedef struct struct_message {
+  uint64_t cycleCnt_u64;
+  int64_t timeSinceBoot_i64;
+	int32_t controllerValue_i32;
+  int8_t pedal_status; //0=default, 1=rudder, 2=rudder brake
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+ESPNow_Send_Struct _ESPNow_Recv;
+ESPNow_Send_Struct _ESPNow_Send;
+void sendMessageToMaster(int32_t controllerValue)
+{
+
+  myData.cycleCnt_u64++;
+  myData.timeSinceBoot_i64 = esp_timer_get_time() / 1000;
+  myData.controllerValue_i32 = controllerValue;
+  if(dap_calculationVariables_st.Rudder_status)
+  {
+    if(dap_calculationVariables_st.rudder_brake_status)
+    {
+      myData.pedal_status=2;
+    }
+    else
+    {
+      myData.pedal_status=1;
+    }
+  }
+  else
+  {
+    myData.pedal_status=0;
+  }
+  esp_now_send(broadcast_mac, (uint8_t *) &myData, sizeof(myData));
+
+  
+  
+  //esp_now_send(esp_master, (uint8_t *) &myData, sizeof(myData));
+  /*
+  if (result != ESP_OK) 
+  {
+    ESPNow_no_device=true;
+    //Serial.println("Failed send data to ESP_Master");
+  }
+  else
+  {
+    ESPNow_no_device=false;
+  }
+  */
+  
+  /*if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }*/
+}
+void onRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) 
+{
+  /*
+  if(ESPNOW_status)
+  {
+    memcpy(&ESPNow_recieve, data, sizeof(ESPNow_recieve));
+    ESPNow_update=true;
+  }
+  */
+  if(ESPNOW_status)
+  {
+    if(data_len==sizeof(_ESPNow_Recv))
+    {
+      memcpy(&_ESPNow_Recv, data, sizeof(_ESPNow_Recv));
+      ESPNow_update=true;
+    }
+    
+    if(data_len==sizeof(dap_config_st))
+    {
+
+      if(mac_addr[5]==esp_Host[5])
+      {
+        //Serial.println("dap_config_st ESPNow recieved");
+        if(semaphore_updateConfig!=NULL)
+        {
+          if(xSemaphoreTake(semaphore_updateConfig, (TickType_t)1)==pdTRUE)
+          {
+            bool structChecker = true;
+            uint16_t crc;
+            DAP_config_st * dap_config_st_local_ptr;
+            dap_config_st_local_ptr = &dap_config_st_local;
+            //Serial.readBytes((char*)dap_config_st_local_ptr, sizeof(DAP_config_st));
+            memcpy(dap_config_st_local_ptr, data, sizeof(DAP_config_st));
+       
+  
+
+            // check if data is plausible
+            if ( dap_config_st_local.payLoadHeader_.payloadType != DAP_PAYLOAD_TYPE_CONFIG )
+            { 
+              structChecker = false;
+
+            }
+            if ( dap_config_st_local.payLoadHeader_.version != DAP_VERSION_CONFIG )
+            { 
+              structChecker = false;
+
+            }
+                    // checksum validation
+            crc = checksumCalculator((uint8_t*)(&(dap_config_st_local.payLoadHeader_)), sizeof(dap_config_st_local.payLoadHeader_) + sizeof(dap_config_st_local.payLoadPedalConfig_));
+            if (crc != dap_config_st_local.payloadFooter_.checkSum)
+            { 
+              structChecker = false;
+
+            }
+
+
+                    // if checks are successfull, overwrite global configuration struct
+            if (structChecker == true)
+            {
+              //Serial.println("Updating pedal config");
+              configUpdateAvailable = true;          
+            }
+              xSemaphoreGive(semaphore_updateConfig);
+          }
+        }
+      }  
+      
+    }
+
+    DAP_actions_st dap_actions_st;
+    if(data_len==sizeof(dap_actions_st))
+    {
+            
+            memcpy(&dap_actions_st, data, sizeof(dap_actions_st));
+            //Serial.readBytes((char*)&dap_actions_st, sizeof(DAP_actions_st));
+            if(dap_actions_st.payLoadHeader_.PedalTag==dap_config_st.payLoadPedalConfig_.pedal_type)
+            {
+              bool structChecker = true;
+              uint16_t crc;
+              if ( dap_actions_st.payLoadHeader_.payloadType != DAP_PAYLOAD_TYPE_ACTION )
+              { 
+                structChecker = false;
+
+              }
+              if ( dap_actions_st.payLoadHeader_.version != DAP_VERSION_CONFIG ){ 
+                structChecker = false;
+
+              }
+              crc = checksumCalculator((uint8_t*)(&(dap_actions_st.payLoadHeader_)), sizeof(dap_actions_st.payLoadHeader_) + sizeof(dap_actions_st.payloadPedalAction_));
+              if (crc != dap_actions_st.payloadFooter_.checkSum){ 
+                structChecker = false;
+
+              }
+
+
+              if (structChecker == true)
+              {
+
+                //1=trigger reset pedal position
+                if (dap_actions_st.payloadPedalAction_.system_action_u8==1)
+                {
+                  resetPedalPosition = true;
+                }
+                //2= restart pedal
+                if (dap_actions_st.payloadPedalAction_.system_action_u8==2)
+                {
+                  ESPNow_restart = true;
+                }
+
+                // trigger ABS effect
+                if (dap_actions_st.payloadPedalAction_.triggerAbs_u8)
+                {
+                  absOscillation.trigger();
+                }
+                //RPM effect
+                _RPMOscillation.RPM_value=dap_actions_st.payloadPedalAction_.RPM_u8;
+                //G force effect
+                _G_force_effect.G_value=dap_actions_st.payloadPedalAction_.G_value-128;       
+                //wheel slip
+                if (dap_actions_st.payloadPedalAction_.WS_u8)
+                {
+                  _WSOscillation.trigger();
+                }     
+                //Road impact
+                _Road_impact_effect.Road_Impact_value=dap_actions_st.payloadPedalAction_.impact_value_u8;
+                // trigger system identification
+                if (dap_actions_st.payloadPedalAction_.startSystemIdentification_u8)
+                {
+                  systemIdentificationMode_b = true;
+                }
+                // trigger Custom effect effect 1
+                if (dap_actions_st.payloadPedalAction_.Trigger_CV_1)
+                {
+                  CV1.trigger();
+                }
+                // trigger Custom effect effect 2
+                if (dap_actions_st.payloadPedalAction_.Trigger_CV_2)
+                {
+                  CV2.trigger();
+                }
+                // trigger return pedal position
+                if (dap_actions_st.payloadPedalAction_.returnPedalConfig_u8)
+                {
+                  ESPNow_config_request=true;
+                  /*
+                  DAP_config_st * dap_config_st_local_ptr;
+                  dap_config_st_local_ptr = &dap_config_st;
+                  //uint16_t crc = checksumCalculator((uint8_t*)(&(dap_config_st.payLoadHeader_)), sizeof(dap_config_st.payLoadHeader_) + sizeof(dap_config_st.payLoadPedalConfig_));
+                  crc = checksumCalculator((uint8_t*)(&(dap_config_st.payLoadHeader_)), sizeof(dap_config_st.payLoadHeader_) + sizeof(dap_config_st.payLoadPedalConfig_));
+                  dap_config_st_local_ptr->payloadFooter_.checkSum = crc;
+                  Serial.write((char*)dap_config_st_local_ptr, sizeof(DAP_config_st));
+                  Serial.print("\r\n");
+                  */
+                }
+                if(dap_actions_st.payloadPedalAction_.Rudder_action==1)
+                {
+                  if(dap_calculationVariables_st.Rudder_status==false)
+                  {
+                    dap_calculationVariables_st.Rudder_status=true;
+                    //Serial.println("Rudder on");
+                    moveSlowlyToPosition_b=true;
+                    //Serial.print("status:");
+                    //Serial.println(dap_calculationVariables_st.Rudder_status);
+                  }
+                  else
+                  {
+                    dap_calculationVariables_st.Rudder_status=false;
+                    //Serial.println("Rudder off");
+                    moveSlowlyToPosition_b=true;
+                    //Serial.print("status:");
+                    //Serial.println(dap_calculationVariables_st.Rudder_status);
+                  }
+                }
+                if(dap_actions_st.payloadPedalAction_.Rudder_brake_action==1)
+                {
+                  if(dap_calculationVariables_st.rudder_brake_status==false&&dap_calculationVariables_st.Rudder_status==true)
+                  {
+                    dap_calculationVariables_st.rudder_brake_status=true;
+                    //Serial.println("Rudder brake on");
+                    //Serial.print("status:");
+                    //Serial.println(dap_calculationVariables_st.Rudder_status);
+                  }
+                  else
+                  {
+                    dap_calculationVariables_st.rudder_brake_status=false;
+                    //Serial.println("Rudder brake off");
+                    //Serial.print("status:");
+                    //Serial.println(dap_calculationVariables_st.Rudder_status);
+                  }
+                }
+              }
+            }
+
+            
+
+          
+            
+    }
+
+  }
+
+}
+void OnSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+
+}
+void ESPNow_initialize()
+{
+
+    WiFi.mode(WIFI_MODE_STA);
+    Serial.println("Initializing ESPNow, please wait"); 
+    Serial.print("Current MAC Address:  ");  
+    Serial.println(WiFi.macAddress());
+    if(dap_config_st.payLoadPedalConfig_.pedal_type==0)
+    {
+      esp_wifi_set_mac(WIFI_IF_STA, &Clu_mac[0]);
+    }
+    if(dap_config_st.payLoadPedalConfig_.pedal_type==1)
+    {
+      esp_wifi_set_mac(WIFI_IF_STA, &Brk_mac[0]);
+    }
+    if(dap_config_st.payLoadPedalConfig_.pedal_type==2)
+    {
+      esp_wifi_set_mac(WIFI_IF_STA, &Gas_mac[0]);
+    }
+    delay(300);
+    Serial.print("Modified MAC Address:  ");  
+    Serial.println(WiFi.macAddress());
+    ESPNow.init();
+    Serial.println("wait  for ESPNOW initialized");
+    delay(3000);
+    esp_wifi_config_espnow_rate(WIFI_IF_STA, WIFI_PHY_RATE_MCS0_LGI);
+    if(dap_config_st.payLoadPedalConfig_.pedal_type==1)
+    {
+      Recv_mac=Gas_mac;      
+    }
+
+    if(dap_config_st.payLoadPedalConfig_.pedal_type==2)
+    {
+      Recv_mac=Brk_mac;
+    }
+    if(ESPNow.add_peer(Recv_mac)== ESP_OK)
+    {
+      ESPNOW_status=true;
+      Serial.println("Sucess to add peer");
+    }
+    else
+    {
+      ESPNOW_status=false;
+      Serial.println("Fail to add peer");
+    }
+    
+    if(ESPNow.add_peer(esp_master)== ESP_OK)
+    {
+      ESPNOW_status=true;
+      Serial.println("Sucess to add host peer");
+    }
+    if(ESPNow.add_peer(esp_Host)== ESP_OK)
+    {
+      ESPNOW_status=true;
+      Serial.println("Sucess to add host peer");
+    }
+    if(ESPNow.add_peer(broadcast_mac)== ESP_OK)
+    {
+      ESPNOW_status=true;
+      Serial.println("Sucess to add broadcast peer");
+    }
+    ESPNow.reg_recv_cb(onRecv);
+    ESPNow.reg_send_cb(OnSent);
+    ESPNow_initial_status=true;
+    Serial.println("ESPNow Initialized");
+  
+}
