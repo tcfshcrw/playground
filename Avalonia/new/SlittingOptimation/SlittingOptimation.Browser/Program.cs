59cf41687156562233a8d51b08fbcf27f0beda8c
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2025-06-06 10:28:20 +0800

    updated

--- Avalonia/new/SlittingOptimation/SlittingOptimation.Browser/Program.cs
+++ Avalonia/new/SlittingOptimation/SlittingOptimation.Browser/Program.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Browser;
-using SlittingOptimation;
+using SlittingOptimization;
 
 internal sealed partial class Program
 {
--- Avalonia/new/SlittingOptimation/SlittingOptimation.Desktop/Program.cs
+++ Avalonia/new/SlittingOptimation/SlittingOptimation.Desktop/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Avalonia;
 
-namespace SlittingOptimation.Desktop;
+namespace SlittingOptimization.Desktop;
 
 sealed class Program
 {
