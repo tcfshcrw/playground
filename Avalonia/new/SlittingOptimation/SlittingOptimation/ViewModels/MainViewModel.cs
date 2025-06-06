7ee6385669d7556ae9bb6cdd0621415ffc414d86
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2025-06-06 10:22:49 +0800

    upload

---
+++ Avalonia/new/SlittingOptimation/SlittingOptimation/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SlittingOptimization.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    public ObservableCollection<CutItem> Items { get; } = new();
+
+    public IRelayCommand AddItemCommand { get; }
+    public IRelayCommand RemoveLastItemCommand { get; }
+
+    public MainViewModel()
+    {
+        AddItemCommand = new RelayCommand(AddItem);
+        RemoveLastItemCommand = new RelayCommand(RemoveLastItem, CanRemoveLastItem);
+
+        // initialize default data
+        Items.Add(new CutItem { Code = "A", Width = 140, Weight = 1, Preferred = false, MaxCount = 3 });
+        Items.Add(new CutItem { Code = "B", Width = 160, Weight = 1, Preferred = false, MaxCount = 4 });
+        Items.Add(new CutItem { Code = "C", Width = 180, Weight = 1, Preferred = false, MaxCount = 4 });
+        Items.Add(new CutItem { Code = "D", Width = 200, Weight = 1, Preferred = false, MaxCount = 4 });
+    }
+
+    private void AddItem() => Items.Add(new CutItem());
+    void RemoveLastItem()
+    {
+        if (Items.Count > 0)
+            Items.RemoveAt(Items.Count - 1);
+    }
+    bool CanRemoveLastItem() => Items.Count > 0;
+    
+    public partial class CutItem : ObservableObject
+    {
+        [ObservableProperty] private string code = "";
+        [ObservableProperty] private int width;
+        [ObservableProperty] private int weight;
+        [ObservableProperty] private bool preferred;
+        [ObservableProperty] private int maxCount;
+    }
+}
+
+
